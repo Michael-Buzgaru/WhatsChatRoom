abfd0502a19e8c49779c6f7018fd2d09495c8923
Michael Buzgaru <39276891+Michael-Buzgaru@users.noreply.github.com>
2018-12-11 13:09:33 +0100

    Delete handler.h

--- include/handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef HANDLER_H_INCLUDED
-#define HANDLER_H_INCLUDED
-
-char *src_to_dest           ( char *const dest, const size_t dest_len, const char *const src, const size_t src_len );
-size_t get_length_null_char ( const char *const src );
-
-#endif // HANDLER_H_INCLUDED

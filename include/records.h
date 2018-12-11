ef990390a6f01dd13e5e4baca01be025e4641421
Michael Buzgaru <39276891+Michael-Buzgaru@users.noreply.github.com>
2018-12-11 13:09:57 +0100

    Delete records.h

--- include/records.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECORDS_H_INCLUDED
-#define RECORDS_H_INCLUDED
-
-void client_join_chat( const struct node *const info_client );
-void client_leave ( const int client_ID );
-void print_online_users ( const int client_ID );
-int search_client_in_list ( const int client_ID );
-
-#endif // RECORDS_H_INCLUDED

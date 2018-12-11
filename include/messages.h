1ecb936c170a9cb67597026bc8d7a00781be3b7c
Michael Buzgaru <39276891+Michael-Buzgaru@users.noreply.github.com>
2018-12-11 13:09:45 +0100

    Delete messages.h

--- include/messages.h
+++
@@ -1,8 +0,0 @@
-#ifndef MESSAGES_H_INCLUDED
-#define MESSAGES_H_INCLUDED
-
-ssize_t send_sckt   ( const int socketID, char *const msg );
-ssize_t recv_sckt ( const int clientID, char *msg );
-void print_msg      ( const char *const where, const char *const src );
-
-#endif // MESSAGES_H_INCLUDED
